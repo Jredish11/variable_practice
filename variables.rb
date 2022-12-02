76372e9a8a2bb5b86da5e967ac2ad7dc44b3dd4e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-12-02 12:16:30 -0700

    Add all puts statements

--- variables.rb
+++ variables.rb
@@ -2,8 +2,16 @@ email = "jredish@gmail.com"
 name = "Jeff"
 age = 33
 amount_paid = 3000.00
-deposit_paid = True
+deposit_paid = true
  
 email = "jredish11@gmail.com"
 
-puts "jredish11@gmail.com"
+
+
+puts "#{email}"
+puts "#{name}"
+puts "#{age}"
+puts "#{amount_paid}"
+puts "#{deposit_paid}"
+
+
